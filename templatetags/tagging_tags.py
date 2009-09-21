8ae60a90daab7e805cfc44677bc764a83dd9f270
tony <tony@malkom.local>
2009-09-21 23:04:39 +0700

    Object nodes for new selection type

--- templatetags/tagging_tags.py
+++ templatetags/tagging_tags.py
@@ -1,4 +1,4 @@
-from django.db.models import get_model
+from django.db.models import get_model, Q
 from django.template import Library, Node, TemplateSyntaxError, Variable, resolve_variable
 from django.utils.translation import ugettext as _
 
@@ -20,20 +20,6 @@ class TagsForModelNode(Node):
         context[self.context_var] = Tag.objects.usage_for_model(model, counts=self.counts)
         return ''
 
-class TagCloudForModelNode(Node):
-    def __init__(self, model, context_var, **kwargs):
-        self.model = model
-        self.context_var = context_var
-        self.kwargs = kwargs
-
-    def render(self, context):
-        model = get_model(*self.model.split('.'))
-        if model is None:
-            raise TemplateSyntaxError(_('tag_cloud_for_model tag was given an invalid model: %s') % self.model)
-        context[self.context_var] = \
-            Tag.objects.cloud_for_model(model, **self.kwargs)
-        return ''
-
 class TagsForObjectNode(Node):
     def __init__(self, obj, context_var):
         self.obj = Variable(obj)
@@ -41,8 +27,38 @@ class TagsForObjectNode(Node):
 
     def render(self, context):
         context[self.context_var] = \
-            Tag.objects.get_for_object(self.obj.resolve(context))
+                Tag.objects.get_for_object(self.obj.resolve(context))
         return ''
+
+class PopularTagsForObjectNode(TagsForObjectNode):
+
+    def render(self, context):
+        context[self.context_var] = \
+                Tag.objects.get_for_object(self.obj.resolve(context), None, items__popular=True)
+        return ''
+
+class TagsForObjectOwner(Node):
+    def __init__(self, obj, owner, context_var):
+        self.obj = Variable(obj)
+        self.owner = Variable(owner)
+        self.context_var = context_var
+
+    def render(self, context):
+
+        context[self.context_var] = \
+                Tag.objects.get_for_object_owner(self.obj.resolve(context), self.owner.resolve(context))
+        return ''
+
+
+class MixedTags(TagsForObjectOwner):
+
+    def render(self, context):
+        owner = self.owner.resolve(context)
+        obj = self.obj.resolve(context)
+        context[self.context_var] = \
+                Tag.objects.get_for_object(obj, owner, Q(items__popular=True) | Q(owners=owner))
+        return ''
+
 
 class TaggedObjectsNode(Node):
     def __init__(self, tag, model, context_var):
@@ -99,86 +115,7 @@ def do_tags_for_model(parser, token):
     else:
         return TagsForModelNode(bits[1], bits[3], counts=True)
 
-def do_tag_cloud_for_model(parser, token):
-    """
-    Retrieves a list of ``Tag`` objects for a given model, with tag
-    cloud attributes set, and stores them in a context variable.
-
-    Usage::
-
-       {% tag_cloud_for_model [model] as [varname] %}
-
-    The model is specified in ``[appname].[modelname]`` format.
-
-    Extended usage::
-
-       {% tag_cloud_for_model [model] as [varname] with [options] %}
-
-    Extra options can be provided after an optional ``with`` argument,
-    with each option being specified in ``[name]=[value]`` format. Valid
-    extra options are:
-
-       ``steps``
-          Integer. Defines the range of font sizes.
-
-       ``min_count``
-          Integer. Defines the minimum number of times a tag must have
-          been used to appear in the cloud.
-
-       ``distribution``
-          One of ``linear`` or ``log``. Defines the font-size
-          distribution algorithm to use when generating the tag cloud.
-
-    Examples::
-
-       {% tag_cloud_for_model products.Widget as widget_tags %}
-       {% tag_cloud_for_model products.Widget as widget_tags with steps=9 min_count=3 distribution=log %}
-
-    """
-    bits = token.contents.split()
-    len_bits = len(bits)
-    if len_bits != 4 and len_bits not in range(6, 9):
-        raise TemplateSyntaxError(_('%s tag requires either three or between five and seven arguments') % bits[0])
-    if bits[2] != 'as':
-        raise TemplateSyntaxError(_("second argument to %s tag must be 'as'") % bits[0])
-    kwargs = {}
-    if len_bits > 5:
-        if bits[4] != 'with':
-            raise TemplateSyntaxError(_("if given, fourth argument to %s tag must be 'with'") % bits[0])
-        for i in range(5, len_bits):
-            try:
-                name, value = bits[i].split('=')
-                if name == 'steps' or name == 'min_count':
-                    try:
-                        kwargs[str(name)] = int(value)
-                    except ValueError:
-                        raise TemplateSyntaxError(_("%(tag)s tag's '%(option)s' option was not a valid integer: '%(value)s'") % {
-                            'tag': bits[0],
-                            'option': name,
-                            'value': value,
-                        })
-                elif name == 'distribution':
-                    if value in ['linear', 'log']:
-                        kwargs[str(name)] = {'linear': LINEAR, 'log': LOGARITHMIC}[value]
-                    else:
-                        raise TemplateSyntaxError(_("%(tag)s tag's '%(option)s' option was not a valid choice: '%(value)s'") % {
-                            'tag': bits[0],
-                            'option': name,
-                            'value': value,
-                        })
-                else:
-                    raise TemplateSyntaxError(_("%(tag)s tag was given an invalid option: '%(option)s'") % {
-                        'tag': bits[0],
-                        'option': name,
-                    })
-            except ValueError:
-                raise TemplateSyntaxError(_("%(tag)s tag was given a badly formatted option: '%(option)s'") % {
-                    'tag': bits[0],
-                    'option': bits[i],
-                })
-    return TagCloudForModelNode(bits[1], bits[3], **kwargs)
-
-def do_tags_for_object(parser, token):
+def do_tags_for_object(parser, token, node=None):
     """
     Retrieves a list of ``Tag`` objects associated with an object and
     stores them in a context variable.
@@ -191,12 +128,42 @@ def do_tags_for_object(parser, token):
 
         {% tags_for_object foo_object as tag_list %}
     """
+
+    if node is None:
+        node = TagsForObjectNode
+    
     bits = token.contents.split()
     if len(bits) != 4:
         raise TemplateSyntaxError(_('%s tag requires exactly three arguments') % bits[0])
     if bits[2] != 'as':
         raise TemplateSyntaxError(_("second argument to %s tag must be 'as'") % bits[0])
-    return TagsForObjectNode(bits[1], bits[3])
+    return node(bits[1], bits[3])
+
+def do_popular_tags_for_object(parser, token, node=None):
+
+    return do_tags_for_object(parser, token, PopularTagsForObjectNode)
+
+def do_mixed_tags_for_object(parser, token):
+    """
+    Retrieves a list of ``Tag`` objects associated with an object and
+    stores them in a context variable.
+
+    Usage::
+
+       {% mixed_tags_for_object [object] [owner] as [varname] %}
+
+    Example::
+
+        {% mixed_tags_for_object foo_object foo_owner as tag_list %}
+    """
+    
+    bits = token.contents.split()
+    if len(bits) != 5:
+        raise TemplateSyntaxError(_('%s tag requires exactly four arguments') % bits[0])
+    if bits[3] != 'as':
+        raise TemplateSyntaxError(_("second argument to %s tag must be 'as'") % bits[0])
+    return MixedTags(bits[1], bits[2], bits[4])
+
 
 def do_tagged_objects(parser, token):
     """
@@ -226,6 +193,7 @@ def do_tagged_objects(parser, token):
     return TaggedObjectsNode(bits[1], bits[3], bits[5])
 
 register.tag('tags_for_model', do_tags_for_model)
-register.tag('tag_cloud_for_model', do_tag_cloud_for_model)
+register.tag('popular_tags_for_object', do_popular_tags_for_object)
 register.tag('tags_for_object', do_tags_for_object)
+register.tag('mixed_tags_for_object', do_mixed_tags_for_object)
 register.tag('tagged_objects', do_tagged_objects)
